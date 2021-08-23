839cb003367a74549314cc7175e8b187327cd6dd
Thiago Cesar de Morais Pedrozo <mr.thiagocesar@hotmail.com>
2021-08-23 16:11:46 -0300

    seo

---
+++ src/components/SEO/index.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import Head from "next/head";
+
+export default function SEO({
+  description = "Encontre e gere seus leads automaticamente. Tenha acesso à todas empresas do mercado pelo menor preço.",
+  author = "BestLead",
+  meta,
+  title = "BestLead - Melhor custo por lead",
+}) {
+  const metaData = [
+    {
+      name: `description`,
+      content: description,
+    },
+    {
+      property: `og:title`,
+      content: title,
+    },
+    {
+      property: `og:description`,
+      content: description,
+    },
+    {
+      property: `og:type`,
+      content: `website`,
+    },
+    {
+      name: `twitter:card`,
+      content: `summary`,
+    },
+    {
+      name: `twitter:creator`,
+      content: author,
+    },
+    {
+      name: `twitter:title`,
+      content: title,
+    },
+    {
+      name: `twitter:description`,
+      content: description,
+    },
+    {
+      name: `keywords`,
+      content: `prospecção de clientes, abordagem ao cliente, captação de clientes, empresa leads, como vender mais e melhor, como vender mais, técnicas de vendas, comprar leads, venda mais, carteira de cliente, lead, leads`,
+    },
+    {
+      name: `theme-color`,
+      content: `#2FC6CE`,
+    },
+  ].concat(meta);
+
+  return (
+    <Head>
+      <title>{title}</title>
+      <link rel="shortcut icon" href="/img/icon-512.png" />
+      <link rel="apple-touch-icon" href="/img/icon-192.png" />
+      <link rel="manifest" href="/manifest.json" />
+      {metaData.map(({ name, content }, i) => (
+        <meta key={i} name={name} content={content} />
+      ))}
+    </Head>
+  );
+}
+
+SEO.defaultProps = {
+  lang: `pt-br`,
+  meta: [],
+};
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,18 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-modal-video/css/modal-video.min.css";
-import Head from "next/head";
+import Seo from '../components/SEO';
 
 export default function CustomApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <title>Best Lead - Site</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-192.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#2FC6CE" />
-        <meta name="description" content="Best Lead" />
-      </Head>
+      <Seo />
       <Component {...pageProps} />
     </>
   );
